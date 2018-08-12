920b571694da46778c52eb6930875e001fecf3dd
douglas_l_brown <douglas_l_brown@apple.com>
2018-08-12 11:00:38 -0700

    Include AddMovement Form

---
+++ client/src/components/_Forms/AddMovement/index.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Col, Form, FormGroup, Label, Input, Button, Alert } from "reactstrap";
+
+import "./style.css";
+
+class FormAddMovement extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      type: ""
+    };
+  }
+
+  onInputChange = (name, value) => {
+    const state = this.state;
+    state[name] = value;
+    this.setState(state);
+  };
+
+  onSubmit = async () => {
+    const { onSubmit } = this.props;
+    const { name, type } = this.state;
+
+    const result = await onSubmit(name, type);
+    if (result) {
+      this.setState({ name: "", type: "" });
+    }
+  };
+
+  render() {
+    const { error, disable } = this.props;
+    const { name, type } = this.state;
+
+    return (
+      <Form className="FormAddMovement">
+        <FormGroup row>
+          <Label for="movementName" hidden>
+            Movement Name
+          </Label>
+          <Col>
+            <Input
+              type="name"
+              name="name"
+              id="name"
+              placeholder="Movement Name"
+              value={name}
+              onChange={e => this.onInputChange("name", e.target.value)}
+              disabled={disable}
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Label for="movementType" hidden>
+            Movement Type
+          </Label>
+          <Input
+            type="select"
+            name="select"
+            id="movementType"
+            onChange={e => this.onInputChange("type", e.target.value)}
+            disabled={disable}
+            value={type}
+            onKeyPress={e => e.key === "Enter" && onSubmit(name, type)}
+          >
+            <option disabled value={""}>
+              Movement Type
+            </option>
+            <option>WEIGHT</option>
+            <option>REPETITIONS</option>
+            <option>DISTANCE</option>
+            <option>HEIGHT</option>
+          </Input>
+        </FormGroup>
+        {error && <Alert color="danger">{error}</Alert>}
+        <Button
+          block
+          disabled={disable || name.length < 3 || type.length === 0}
+          onClick={() => this.onSubmit(name, type)}
+        >
+          Login
+        </Button>
+      </Form>
+    );
+  }
+}
+
+FormAddMovement.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  error: PropTypes.string,
+  disable: PropTypes.bool
+};
+
+FormAddMovement.defaultProps = {
+  error: undefined,
+  disable: false
+};
+
+export default FormAddMovement;
